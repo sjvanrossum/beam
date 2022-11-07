da22171523c923f7dbca4fce372bcc7cf534c0cb
Nivaldo Tokuda <ni.h1@hotmail.com>
2022-11-06 23:23:24 -0300

    Add protos to build

---
+++ sdks/rust/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# TODO: make build.rs use relative paths and stop copying the protos from Beam
+# root
+
+cd src
+rm -r beam_protos/
+mkdir beam_protos
+cp -r ../../../model beam_protos
+cd ..
+cargo build

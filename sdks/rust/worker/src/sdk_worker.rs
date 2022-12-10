d8984419ae378a13d40d94dd08ca65a2a36ade08
Nivaldo Tokuda <ni.h1@hotmail.com>
2022-12-10 18:01:47 -0300

    Starting BundleProcessor construction

--- sdks/rust/worker/src/sdk_worker.rs
+++ sdks/rust/worker/src/sdk_worker.rs
@@ -20,6 +20,7 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use http::Uri;
+use proto::beam::pipeline::PTransform;
 use tonic::transport::Channel;
 
 use proto::beam::fn_execution::{
@@ -28,14 +29,18 @@ use proto::beam::fn_execution::{
     ProcessBundleSplitResponse,
 };
 
-use crate::operators::{create_operator, IOperator, OperatorContext, Receiver};
+use crate::operators::{create_operator, Operator, OperatorContext, Receiver};
 
 #[derive(Debug)]
 pub struct Worker {
+    control_client: Arc<BeamFnControlClient<Channel>>,
+
+    process_bundle_descriptors: HashMap<String, ProcessBundleDescriptor>,
+    bundle_processors: HashMap<String, BundleProcessor>,
+    active_bundle_processors: HashMap<String, BundleProcessor>,
     id: String,
     endpoints: WorkerEndpoints,
     options: HashMap<String, String>,
-    control_client: Arc<BeamFnControlClient<Channel>>,
 }
 
 impl Worker {
@@ -47,10 +52,14 @@ impl Worker {
             .expect("Failed to connect to worker");
 
         Mutex::new(Self {
+            control_client: Arc::new(BeamFnControlClient::new(channel)),
+
+            process_bundle_descriptors: HashMap::new(),
+            bundle_processors: HashMap::new(),
+            active_bundle_processors: HashMap::new(),
             id,
             endpoints,
             options: HashMap::new(),
-            control_client: Arc::new(BeamFnControlClient::new(channel)),
         })
     }
 
@@ -80,6 +89,119 @@ impl WorkerEndpoints {
 #[derive(Debug)]
 pub struct BundleProcessor {
     descriptor: ProcessBundleDescriptor,
-    topologically_ordered_operators: Vec<Box<dyn IOperator>>,
-    operators: HashMap<String, Box<dyn IOperator>>,
+
+    creation_ordered_operators: Mutex<Vec<Operator>>,
+    topologically_ordered_operators: Vec<Operator>,
+    operators: Mutex<HashMap<String, Operator>>,
+    receivers: Mutex<HashMap<String, Receiver>>,
+    consumers: Mutex<HashMap<String, Vec<String>>>,
+
+    current_bundle_id: Option<String>,
+}
+
+impl BundleProcessor {
+    pub fn new(descriptor: ProcessBundleDescriptor, root_urns: &[&'static str]) -> Self {
+        let mut consumers: HashMap<String, Vec<String>> = HashMap::new();
+        for (transform_id, ptransform) in descriptor.transforms.iter() {
+            if is_primitive(ptransform) {
+                ptransform
+                    .inputs
+                    .values()
+                    .for_each(|pcollection_id: &String| {
+                        if (consumers.get(pcollection_id).is_none()) {
+                            consumers.insert(pcollection_id.clone(), Vec::new());
+                        }
+
+                        consumers
+                            .get_mut(pcollection_id)
+                            .unwrap()
+                            .push(transform_id.clone());
+                    })
+            }
+        }
+
+        let mut _instance = Self {
+            descriptor: descriptor.clone(),
+            creation_ordered_operators: Mutex::new(Vec::new()),
+            topologically_ordered_operators: Vec::new(),
+            operators: Mutex::new(HashMap::new()),
+            receivers: Mutex::new(HashMap::new()),
+            consumers: Mutex::new(consumers),
+            current_bundle_id: None,
+        };
+
+        descriptor
+            .transforms
+            .iter()
+            .for_each(|(transform_id, transform)| {
+                if let Some(spec) = &transform.spec {
+                    if root_urns.contains(&spec.urn.as_str()) {
+                        _instance.get_operator(transform_id.clone());
+                    }
+                }
+            });
+
+        _instance.create_topologically_ordered_operators();
+
+        _instance
+    }
+
+    pub fn get_receiver(&self, pcollection_id: String) -> Receiver {
+        let mut receivers = self.receivers.lock().unwrap();
+        let consumers = self.consumers.lock().unwrap();
+
+        receivers.entry(pcollection_id.clone()).or_insert_with(|| {
+            let pcoll_operators: Vec<Operator> = match consumers.get(&pcollection_id) {
+                Some(v) => {
+                    let mut oprs = Vec::new();
+                    for transform_id in v {
+                        oprs.push(self.get_operator(transform_id.clone()))
+                    }
+                    oprs
+                }
+                None => Vec::new(),
+            };
+
+            Receiver::new(pcoll_operators)
+        });
+        receivers[&pcollection_id].clone()
+    }
+
+    pub fn get_operator(&self, transform_id: String) -> Operator {
+        let mut operators = self.operators.lock().unwrap();
+        let get_receiver = |pcollection_id: String| self.get_receiver(pcollection_id);
+
+        operators.entry(transform_id.clone()).or_insert_with(|| {
+            create_operator(
+                &transform_id,
+                OperatorContext {
+                    descriptor: &self.descriptor,
+                    get_receiver: Box::new(get_receiver),
+                },
+            )
+        });
+
+        let mut creation_ordered_operators = self.creation_ordered_operators.lock().unwrap();
+        creation_ordered_operators.push(operators[&transform_id].clone());
+
+        operators[&transform_id].clone()
+    }
+
+    fn create_topologically_ordered_operators(&mut self) {
+        let creation_ordered = self.creation_ordered_operators.lock().unwrap().clone();
+        self.topologically_ordered_operators = creation_ordered.into_iter().rev().collect();
+    }
+}
+
+// TODO
+fn is_primitive(transform: &PTransform) -> bool {
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_operator_construction() {}
 }

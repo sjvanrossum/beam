f3ce3bc313e3ec59c48917b0bb7c088f80b695c8
Nivaldo Tokuda <ni.h1@hotmail.com>
2022-12-10 18:01:47 -0300

    Starting BundleProcessor construction

--- sdks/rust/worker/src/operators.rs
+++ sdks/rust/worker/src/operators.rs
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use std::any::Any;
 use std::collections::HashMap;
 use std::fmt;
@@ -17,34 +35,59 @@ static OPERATORS_BY_URN: Lazy<Mutex<OperatorMap>> = Lazy::new(|| {
     Mutex::new(m)
 });
 
-pub fn create_operator(transform_id: &str, context: OperatorContext) -> Box<dyn IOperator> {
-    let transform = context
-        .descriptor
-        .transforms
-        .get(transform_id)
-        .expect("Transform ID not found");
+#[derive(Clone, fmt::Debug)]
+pub struct Operator {
+    data: OperatorData,
+    receivers: Vec<Receiver>,
+}
 
-    // Ensure receivers are eagerly created
-    for pcoll_id in transform.outputs.values() {
-        (context.get_receiver)(pcoll_id.clone());
-    }
+#[derive(Clone, fmt::Debug)]
+pub enum OperatorData {
+    Create(CreateOperator),
+    Recording(RecordingOperator),
+}
 
-    let operators_by_urn = OPERATORS_BY_URN.lock().unwrap();
+#[derive(Clone, fmt::Debug)]
+pub struct CreateOperator {
+    // TODO: placeholder for Any
+    data: String,
+}
 
-    let spec = transform
-        .spec
-        .as_ref()
-        .unwrap_or_else(|| panic!("Transform {} has no spec", transform_id));
+#[derive(Clone, fmt::Debug)]
+pub struct RecordingOperator {
+    log: Vec<String>,
+    transform_id: String,
+}
 
-    let op = operators_by_urn
-        .get(&spec.urn)
-        .unwrap_or_else(|| panic!("Unknown transform type: {}", spec.urn));
+pub fn create_operator(transform_id: &str, context: OperatorContext) -> Operator {
+    unimplemented!()
 
-    op.new_op(transform_id, transform, &context)
+    // let transform = context
+    //     .descriptor
+    //     .transforms
+    //     .get(transform_id)
+    //     .expect("Transform ID not found");
+
+    // for pcoll_id in transform.outputs.values() {
+    //     (context.get_receiver)(pcoll_id.clone());
+    // }
+
+    // let operators_by_urn = OPERATORS_BY_URN.lock().unwrap();
+
+    // let spec = transform
+    //     .spec
+    //     .as_ref()
+    //     .unwrap_or_else(|| panic!("Transform {} has no spec", transform_id));
+
+    // let op = operators_by_urn
+    //     .get(&spec.urn)
+    //     .unwrap_or_else(|| panic!("Unknown transform type: {}", spec.urn));
+
+    // op.new_op(transform_id, transform, &context)
 }
 
 #[allow(clippy::new_ret_no_self)]
-pub trait IOperator {
+pub trait IOperator: Send {
     fn new_op(
         &self,
         transform_id: &str,
@@ -65,21 +108,21 @@ impl fmt::Debug for dyn IOperator {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Receiver {
-    operators: Vec<Box<dyn IOperator>>,
+    operators: Vec<Operator>,
 }
 
 impl Receiver {
-    pub fn new(operators: Vec<Box<dyn IOperator>>) -> Self {
+    pub fn new(operators: Vec<Operator>) -> Self {
         Receiver { operators }
     }
 
-    pub fn receive(&self, wvalue: &WindowedValue) {
-        for op in &self.operators {
-            op.process(wvalue);
-        }
-    }
+    // pub fn receive(&self, wvalue: &WindowedValue) {
+    //     for op in &self.operators {
+    //         op.process(wvalue);
+    //     }
+    // }
 }
 
 #[derive(Debug)]
@@ -87,9 +130,13 @@ pub struct WindowedValue {
     value: Box<dyn Any>,
 }
 
-pub struct OperatorContext {
-    descriptor: ProcessBundleDescriptor,
-    get_receiver: fn(String) -> Receiver,
-    get_data_channel: fn(&str) -> MultiplexingDataChannel,
-    get_bundle_id: String,
+pub struct OperatorContext<'bundle_processor> {
+    pub descriptor: &'bundle_processor ProcessBundleDescriptor,
+    pub get_receiver: Box<dyn Fn(String) -> Receiver + 'bundle_processor>,
+    // get_data_channel: fn(&str) -> MultiplexingDataChannel,
+    // get_bundle_id: String,
 }
+
+// ******* Operator definitions *******
+
+pub mod test_operators {}

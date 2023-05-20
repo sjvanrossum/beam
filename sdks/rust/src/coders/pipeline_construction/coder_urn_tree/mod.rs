313dcb94b6b07fde630c6585e1356740d55daf33
Sho Nakatani <lay.sakura@gmail.com>
2023-05-20 11:05:35 +0900

    chore: add CoderUrnTree::from_proto stub

--- sdks/rust/src/coders/pipeline_construction/coder_urn_tree/mod.rs
+++ sdks/rust/src/coders/pipeline_construction/coder_urn_tree/mod.rs
@@ -1,6 +1,15 @@
+use crate::proto::pipeline_v1;
+
 /// A coder's URN and URNs of its component coders.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct CoderUrnTree {
     pub(crate) coder_urn: &'static str,
     pub(crate) component_coder_urns: Vec<Self>,
+}
+
+impl CoderUrnTree {
+    /// Recursively construct a `CoderUrnTree`.
+    pub(crate) fn from_proto(root_coder_id: &str, pipeline_proto: &pipeline_v1::Pipeline) -> Self {
+        todo!()
+    }
 }
